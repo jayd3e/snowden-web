e1a4bb9b13524fd15f877733c9ef0812739660be
Joseph Dallago <jd.dallago@gmail.com>
2018-08-19 17:42:58 +0000

    Added functionality around adding messages.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,11 @@
 $(function () {
 	var Modal = window.vex;
 
+	var pubnub = new PubNub({
+		publishKey : 'pub-c-121bd49e-d8bc-4ca0-8a70-e86e447118e5',
+		subscribeKey : 'sub-c-9a2966f0-a3cf-11e8-bb88-163ac01f2f4e'
+	});
+
 	const baseUrl = "https://snowden-api.herokuapp.com";
 
 	var ConversationModel = Backbone.Model.extend();
@@ -15,10 +20,12 @@ $(function () {
 			this.createConversationButtonEl = this.$el.find(".js-create-conversation-button");
 
 			$.ajax({
-				url: baseUrl + "/web/index.php?type=conversations",
+				url: baseUrl + "/index.php?type=conversations",
 				dataType: "json",
 				success: this.initialLoad
 			})
+
+			this.$el.show();
 
 			this.createConversationButtonEl.click(this.createConversation);
 		},
@@ -26,9 +33,10 @@ $(function () {
 			_.each(response, function(conversation) {
 				this.conversationTemplate = _.template(this.conversationTemplateEl.html());
 
+				var model = new ConversationModel(conversation);
 				var conversationView = new ConversationItemView({
-					el: this.conversationTemplate(),
-					model: new ConversationModel(conversation)
+					el: this.conversationTemplate(model.toJSON()),
+					model: model
 				})
 
 				this.conversationsListEl.append(conversationView.el);
@@ -56,21 +64,29 @@ $(function () {
 
 	var ConversationView = Backbone.View.extend({
 		initialize: function(options) {
-			_.bindAll(this, 'initialLoad');
+			_.bindAll(this, 'initialLoad', 'submit');
 
 			/* Elements */
 			this.messageTemplateEl = $("#message-template");
 			this.messagesListEl = this.$el.find('.js-messages-list');
+			this.messagesContentInputEl = this.$el.find('.js-messages-content-input');
+			this.submitEl = this.$el.find('.js-submit');
 
 			this.$el.show();
 
 			$.ajax({
-				url: baseUrl + "/web/index.php?type=conversations&id=" + options.id,
+				url: baseUrl + "/index.php?type=conversations&id=" + options.id,
+				contentType: "application/json",
 				dataType: "json",
 				success: this.initialLoad
-			})
+			});
+
+			this.submitEl.click(this.submit);
 		},
 		initialLoad: function(response) {
+			this.model = new ConversationModel(response[0]);
+
+			this.messagesListEl.empty();
 			_.each(response[0].messages, function(message) {
 				this.messageTemplate = _.template(this.messageTemplateEl.html());
 				this.messagesListEl.append(this.messageTemplate(message));
@@ -81,6 +97,42 @@ $(function () {
 
 			/* Events */
 			this.messageEls.click(this.clickMessage);
+
+			/* PubNub */
+			pubnub.addListener({
+				status: function(statusEvent) {
+					console.log(statusEvent.category);
+				},
+				message: _.bind(function(msg) {
+					$.ajax({
+						url: baseUrl + "/index.php?type=conversations&id=" + this.model.id,
+						contentType: "application/json",
+						dataType: "json",
+						success: this.initialLoad
+					});
+				}, this),
+				presence: function() {
+
+				}
+			})
+			pubnub.subscribe({
+				channels: ['conversations:' + this.model.id]
+			});
+		},
+		submit: function() {
+			$.ajax({
+				method: "POST",
+				url: baseUrl + "/index.php?type=messages",
+				contentType: "application/json",
+				dataType: "json",
+				data: JSON.stringify({
+					"content": this.messagesContentInputEl.val(),
+					"user_id": "1",
+					"conversation_id": this.model.id
+				})
+			});
+
+			return false;
 		}
 	});
 
@@ -143,7 +195,7 @@ $(function () {
 
 	var ExpirationConversationView = Backbone.View.extend({
 		initialize: function() {
-			_.bindAll(this, 'submit');
+			_.bindAll(this, 'submit', 'submitSuccess');
 
 			/* Elements */
 			this.submitEl = this.$el.find('.js-submit');
@@ -153,6 +205,16 @@ $(function () {
 			this.submitEl.click(this.submit);
 		},
 		submit: function() {
+			$.ajax({
+				method: "POST",
+				url: baseUrl + "/index.php?type=conversations",
+				contentType: "application/json",
+				dataType: "json",
+				data: JSON.stringify(this.model.toJSON()),
+				success: this.submitSuccess
+			});
+		},
+		submitSuccess: function() {
 			router.navigate("conversations", {trigger: true});
 
 			this.$el.hide();
@@ -163,6 +225,7 @@ $(function () {
 	var Router = Backbone.Router.extend({
 		routes : {
 			"": "conversations",
+			"conversations": "conversations",
 			"conversation/create": "createConversation",
 			"conversation/start": "startConversation",
 			"conversation/complete": "completeConversation",
@@ -173,39 +236,45 @@ $(function () {
 
 		},
 		conversations: function() {
+			$('.page').hide();
 			var conversationsView = new ConversationsView({
-			  el: $('.js-conversations')
+				el: $('.js-conversations')
 			});
 		},
 		conversation: function(id) {
+			$('.page').hide();
 			var conversationView = new ConversationView({
-			  el: $('.js-messages'),
-			  id: id
+				el: $('.js-messages')	,
+				id: id
 			});
 		},
 		createConversation: function(id) {
+			$('.page').hide();
 			this.conversationModel = new ConversationModel();
 			var createConversationView = new CreateConversationView({
-			  el: $('.js-create-conversation'),
-			  model: this.conversationModel
+				el: $('.js-create-conversation'),
+				model: this.conversationModel
 			});
 		},
 		startConversation: function(id) {
+			$('.page').hide();
 			var createConversationView = new StartConversationView({
-			  el: $('.js-create-conversation-start'),
-			  model: this.conversationModel
+				el: $('.js-create-conversation-start'),
+				model: this.conversationModel
 			});
 		},
 		completeConversation: function(id) {
+			$('.page').hide();
 			var completeConversationView = new CompleteConversationView({
-			  el: $('.js-create-conversation-complete'),
-			  model: this.conversationModel
+				el: $('.js-create-conversation-complete'),
+				model: this.conversationModel
 			});
 		},
 		expirationConversation: function(id) {
+			$('.page').hide();
 			var expirationConversationView = new ExpirationConversationView({
-			  el: $('.js-create-conversation-expiration'),
-			  model: this.conversationModel
+				el: $('.js-create-conversation-expiration'),
+				model: this.conversationModel
 			});
 		}
 	});
