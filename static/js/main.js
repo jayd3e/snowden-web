fe0e83c4dc5344b44556d5f1ff3a5a0f8d6531e7
Pieter Gorsira <pgorsira@gmail.com>
2018-08-19 12:48:02 -0700

    try to dfix slider

--- static/js/main.js
+++ static/js/main.js
@@ -234,7 +234,8 @@ $(function () {
 	var sliderEls = $('.range-slider');
 
 	sliderEls.each(function(){
-		$(this).children('.range-slider-range').on('input', function(){
+		$(this).children('.range-slider-range').on('input', function(event){
+			event.preventDefault();
 			var value = $(this).parent('.range-slider').children('.range-slider-value');
 			value.html(this.value + "%");
 		});
