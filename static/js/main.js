3d6fc860678931fdde3cc789d5f0bd69ee60d012
Joseph Dallago <jd.dallago@gmail.com>
2018-08-19 06:25:54 +0000

    Continued work on the different pages of the frontend.

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
+	var Modal = window.vex;
+
+	var ModalView = Backbone.View.extend({
+		initialize: function() {
+
+		}
+	})
+
+	var modalTemplateEl = $('#address-modal-template');
+	var modalTemplate = _.template(modalTemplateEl.html());
+	var view = new ModalView({
+		el: modalTemplate(),
+	});
+	var modalId = Modal.open({
+			content: view.el
+	});
 
 })
